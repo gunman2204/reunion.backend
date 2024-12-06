525d663d5ee561d79289ed5b654f412b73e9c862
AMAN KUMAR VERMA <vermaaman714600@gmail.com>
2024-12-06 22:33:16 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/User')
 const Task = require('../models/Task')
 const jwt = require('jsonwebtoken');
 // const dotenv = require('.env');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
 // dotenv.config();
 const generateToken = (id) => {
